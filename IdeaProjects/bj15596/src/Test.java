c7e96edb6c2e4f61cb6b3a326163a54dffa71bf7
gudals7856 <gudals7856@gmail.com>
2021-03-17 17:16:01 +0900

    백준 4673 추가

---
+++ IdeaProjects/bj15596/src/Test.java
@@ -0,0 +1,20 @@
+package bj15596;
+
+public class Test {
+
+    static long sum(int[] a) {
+        long ans = 0;
+        int size = a.length;
+        for(int i=0; i<size; i++)
+        {
+            ans += a[i];
+        }
+        System.out.println(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 5, 8};
+        sum(a);
+    }
+}
