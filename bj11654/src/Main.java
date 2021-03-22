be8253446a1a2a142833e3edc591e36b1c04ea0f
gudals7856 <gudals7856@gmail.com>
2021-03-22 16:01:01 +0900

    백준 11654번 풀이

---
+++ bj11654/src/Main.java
@@ -0,0 +1,12 @@
+package bj11654;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.nextLine().charAt(0);
+        int n = (int)c;
+
+        System.out.println(n);
+    }
+}
