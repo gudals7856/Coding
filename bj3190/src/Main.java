acbc6db859d733d47350ffc8ec2e02db48071539
gudals7856 <gudals7856@gmail.com>
2021-07-13 19:03:42 +0900

    백준 3190번(뱀) 풀이

--- bj3190/src/Main.java
+++ bj3190/src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.io.*;
 import java.util.StringTokenizer;
 import java.util.Vector;
@@ -15,6 +16,7 @@ public class Main {
         int time = 0;
         int[][] board = new int[n][n];        // 뱀이 위치되어있으면 1, 아니면 0
         int[][] applePos = new int[k][2];     // 사과의 위치
+        Boolean eatApple = false;
         Vector<Pair> snake = new Vector<Pair>();    // 뱀이 지나온 위치 저장
 
         int addX = 1;   // 뱀이 늘어나는 방향 중 X축
@@ -23,8 +25,10 @@ public class Main {
         for (int i = 0; i < k; i++) {
             String str = br.readLine();
             StringTokenizer st = new StringTokenizer(str);
-            applePos[i][0] = Integer.parseInt(st.nextToken());
-            applePos[i][1] = Integer.parseInt(st.nextToken());
+            applePos[i][0] = Integer.parseInt(st.nextToken()) - 1;
+            applePos[i][1] = Integer.parseInt(st.nextToken()) - 1;
+            System.out.println();
+            System.out.println("apple : " + applePos[i][0] + " " + applePos[i][1]);
         }
 
         int l = Integer.parseInt(br.readLine());
@@ -43,55 +47,89 @@ public class Main {
         snake.add(new Pair(0, 0));
         snake.add(new Pair(0, 1));
 
-        while (X <= n || Y <= n) {      // 벽에 부딪히면 끝남
+        while (true) {      // 벽에 부딪히면 끝남
+
+            // 뱀의 방향 변환 정보 확인
             for (int i = 0; i < l; i++) {
                 if (Integer.parseInt(changeDir[i][0]) == time) {
                     // 뱀의 이동방향 전환
-                    if (changeDir[i][1] == "L") {
+                    if (changeDir[i][1].equals("L")) {
                         if (addX == 1 && addY == 0) {
                             addX = 0;
-                            addY = 1;
+                            addY = -1;
                         }
                         else if (addX == 0 && addY == 1) {
-                            addX = -1;
+                            addX = 1;
                             addY = 0;
                         }
                         else if (addX == -1 && addY == 0) {
                             addX = 0;
-                            addY = -1;
+                            addY = 1;
                         }
                         else if (addX == 0 && addY == -1) {
-                            addX = 1;
+                            addX = -1;
                             addY = 0;
                         }
                     }
-                    else {  // "D" 인 경우
+                    else if (changeDir[i][1].equals("D")) {
                         if (addX == 1 && addY == 0) {
                             addX = 0;
-                            addY = -1;
-                        }
-                        else if (addX == 0 && addY == 1) {
-                            addX = 1;
-                            addY = 0;
-                        }
-                        else if (addX == -1 && addY == 0) {
-                            addX = 0;
                             addY = 1;
-                        }
-                        else if (addX == 0 && addY == -1) {
+                        } else if (addX == 0 && addY == 1) {
                             addX = -1;
+                            addY = 0;
+                        } else if (addX == -1 && addY == 0) {
+                            addX = 0;
+                            addY = -1;
+                        } else if (addX == 0 && addY == -1) {
+                            addX = 1;
                             addY = 0;
                         }
                     }
                 }
             }
 
-
+            System.out.println("좌표 :" + Y + " " + X);
+            X += addX;
+            Y += addY;
             time++;
-            if(board[Y][X] == 1)    // 뱀이 자기 자신에 부딪히면 끝남
-                break;
-        }
 
+            // 사과를 먹는지 확인
+            for (int i = 0; i < k; i++) {
+                if (applePos[i][0] == Y && applePos[i][1] == X) {   // 현재 뱀이 사과의 위치에 있다면
+                    eatApple = true;    // 사과를 먹었다고 표시해줌
+                    applePos[i][0] = n;
+                    applePos[i][1] = n;
+                }
+            }
+
+            // 뱀이 벽에 부딪히면 끝남
+            if (X >= n || X < 0 || Y >= n || Y < 0) {
+                break;
+            }
+            // 뱀이 자기 자신에 부딪히면 끝남
+            if(board[Y][X] == 1)
+                break;
+
+            board[Y][X] = 1;
+
+            // 뱀의 새로운 위치를 저장
+            snake.add(new Pair(Y, X));
+
+            // 뱀이 사과를 먹지 않았다면
+            if (eatApple == false) {
+                board[snake.get(0).first][snake.get(0).second] = 0;    // 뱀의 길이를 줄여줌
+                snake.remove(0);
+            }
+
+            System.out.println(time);
+            for (int i = 0; i < snake.size(); i++) {
+                System.out.println("snake = " + snake.get(i).first + " " + snake.get(i).second);
+            }
+            System.out.println();
+
+            eatApple = false;   // 사과 먹었는지 여부를 false로 바꾸어놓음
+        }
         bw.write(time+"");
         bw.flush();
         bw.close();
@@ -103,7 +141,5 @@ public class Main {
             this.first = f;
             this.second = s;
         }
-
     }
-
 }
