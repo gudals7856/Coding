bdf643b9345aac12f32e095119c9128d85cb9029
gudals7856 <gudals7856@gmail.com>
2021-08-02 17:42:56 +0900

    백준 11729번(하노이 탑) 풀이

--- bj11729/src/Main.java
+++ bj11729/src/Main.java
@@ -1,32 +1,41 @@
 import java.io.*;
 
+
+// BufferedWriter는 안되고 StringBuilder 는 된다??????? String은 시간초과.
+
 public class Main {
 
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int count = 0;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        bw.write(((int)Math.pow(2, n) - 1) +"\n");
-
         hanoi(n, "1", "3", "2");
+        System.out.println(count);
+        System.out.println(sb);
 
-        bw.flush();
-        bw.close();
+//        bw.flush();
+//        bw.close();
 
     }
 
     public static void hanoi(int n, String start, String dest, String assist) throws IOException {
         if(n==1)
         {
-            bw.write(start+" "+dest+"\n");
+            sb.append(start + " " + dest + "\n");
+//            bw.write(start+" "+dest+"\n");
+            count++;
             return;
         }
         else
         {
             hanoi(n-1, start, assist, dest);
-            bw.write(start+" "+dest+"\n");
+            sb.append(start + " " + dest + "\n");
+//            bw.write(start+" "+dest+"\n");
+            count++;
             hanoi(n-1, assist, dest, start);
         }
     }
