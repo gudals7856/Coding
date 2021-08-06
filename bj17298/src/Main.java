8a50d4b0d7aca00372e0be7ccd9eca94a7d852bd
gudals7856 <gudals7856@gmail.com>
2021-08-06 18:58:11 +0900

    백준 17298번(오큰수) 풀이

--- bj17298/src/Main.java
+++ bj17298/src/Main.java
@@ -1,38 +1,50 @@
+/*
+백준 17298번
+오큰수 (스택)
+ */
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static int n;
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
     static int[] num;
-    static int[] nge;
-    static boolean[] check;
+    static Stack<Integer> st = new Stack<>();
 
     public static void main(String[] args) throws Exception {
         n = Integer.parseInt(br.readLine());
         num = new int[n];
-        nge = new int[n];
-        check = new boolean[n];
-        Arrays.fill(nge, -1);
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            num[i] = Integer.parseInt(str.nextToken());
 
-        num[0] = Integer.parseInt(st.nextToken());
+        // 시작할 때 수열의 가장 앞 수의 인덱스를 스택의 넣고 시작한다.
+        st.push(0);
+        
+        // 수들을 모두 확인하면 진행
         for (int i = 1; i < n; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
 
-            for (int j = 0; j < i; j++) {
-                if (num[j] < num[i] && !check[j]) {
-                    nge[j] = num[i];
-                    check[j] = true;
-                }
+            // 스택이 비어있지 않다면 현재 num과 스택 내의 수들을 비교한다.
+            // 스택 안의 수들과 비교하다가 현재 num이 더 작으면 break, 크면 해당 인덱스 수를 현재 num으로 바꿈
+            while (!st.isEmpty()) {
+                if (num[i] <= num[st.peek()])
+                    break;
+                else
+                    num[st.pop()] = num[i];
             }
+            st.push(i);
         }
 
+        // 스택에 남아있는 인덱스에 해당하는 수들을 -1로 저장
+        while (!st.isEmpty())
+            num[st.pop()] = -1;
+        
         for (int i = 0; i < n; i++) {
-            bw.write(nge[i] + " ");
+            bw.write(num[i] + " ");
         }
         bw.flush();
         bw.close();
