ae6ba3ad865816d48e305fbc697f7455cc855205
gudals7856 <gudals7856@gmail.com>
2021-08-31 19:24:56 +0900

    백준 1992번(쿼드트리) 풀이

--- bj1992/src/Main.java
+++ bj1992/src/Main.java
@@ -12,39 +12,39 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        board = new int[n][n];
 
+        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            String[] str = br.readLine().split("");
+            st = new StringTokenizer(br.readLine(), "");
             for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(str[j]);
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        func(0, 0, n);
+        for (int i = 0; i < n; i++) {
+            Arrays.toString(board[i]);
+        }
     }
 
     private static void func(int r, int c, int length) {
         int sum = 0;
+        if (length != n) System.out.print("(");
 
-        // 현재 확인하는 부분에 있는 수들을 모두 더함
         for (int i = r; i < r + length; i++) {
             for (int j = c; j < c + length; j++) {
                 if (board[i][j] == 1) sum++;
             }
         }
 
-        // 합이 확인하는 부분의 크기와 같으면 1출력, 합이 0이면 0출력
         if (sum == length * length) System.out.print(1);
-        else if(sum == 0) System.out.print(0);
-        else {  // 압축이 되지 않는다면 다시 구간을 나눔
+        else if(sum == 0) System.out.println(0);
+        else {
             int next = length / 2;
-            System.out.print("(");
             func(r, c, next);
             func(r, c + next, next);
             func(r + next, c, next);
             func(r + next, c + next, next);
-            System.out.print(")");
         }
+
+        if (length != n) System.out.print(")");
     }
 }
